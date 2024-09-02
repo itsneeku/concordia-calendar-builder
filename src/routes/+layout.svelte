b91bba971f89105544a337a4e84b07db8198f5bf
itsneeku <nicolae@rusu.dev>
2024-09-01 21:36:30 -0400

    feat: :sparkles: display components under layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import Header from './Header.svelte';
 </script>
 
-<slot></slot>
+<div class="app">
+	<Header></Header>
+
+	<main>
+		<slot></slot>
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		padding: 0.5rem;
+	}
+
+	main {
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+	}
+</style>
