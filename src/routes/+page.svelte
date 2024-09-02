8fe5762449065bd3346a355998185dd840753cdc
itsneeku <nicolae@rusu.dev>
2024-09-01 21:35:47 -0400

    feat: :sparkles: add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Roller from './Roller.svelte';
+	const calendars = ['Google Calendar', 'Apple Calendar', 'Microsoft Outlook', 'Samsung Calendar'];
+</script>
+
+<div class="justify-center text-center">
+	<h1 class="mb-4 text-center text-5xl font-semibold tracking-tight lg:text-5xl">
+		Concordia .ics Builder
+	</h1>
+	<div>
+		<p>Easily add your Concordia schedule to</p>
+		<Roller data={calendars} interval={2000} />
+	</div>
+</div>
+
+<style>
+</style>
