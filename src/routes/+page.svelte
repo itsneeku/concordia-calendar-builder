beff7107ac44d41329447874e5897b012d8e37ed
itsneeku <nicolae@rusu.dev>
2024-09-01 23:08:27 -0400

    feat: :lipstick: add styled input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 	import Roller from './Roller.svelte';
+	import { Input } from '$lib/components/ui/input';
 	const calendars = ['Google Calendar', 'Apple Calendar', 'Microsoft Outlook', 'Samsung Calendar'];
 </script>
 
-<div class="justify-center text-center">
-	<h1 class="mb-4 text-center text-5xl font-semibold tracking-tight lg:text-5xl">
+<section
+	style="flex: 0.25;"
+	class="wrapper flex flex-auto flex-col items-center justify-center text-center"
+>
+	<h1 class="mb-4 p-2 text-center text-5xl font-semibold tracking-tight lg:text-5xl">
 		Concordia .ics Builder
 	</h1>
-	<div>
+	<div class="mb-8">
 		<p>Easily add your Concordia schedule to</p>
 		<Roller data={calendars} interval={2000} />
 	</div>
-</div>
+
+	<Input class="  max-w-80" id="scheduleInput" placeholder="Paste your schedule"></Input>
+</section>
 
 <style>
 </style>
