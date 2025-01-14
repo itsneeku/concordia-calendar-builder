f91c95a84e08fa093499e23e70f64f08cfae0809
Nicolae Rusu <nicolae@rusu.dev>
2025-01-13 22:52:18 -0500

    chore: upgrade and cf pages + workers setup

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,11 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Platform {
+			env?: {
+				KV: KVNamespace;
+			};
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,22 +6,19 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// pages: 'build',
-			// assets: 'build',
-			// fallback: '404.html'
-			//1 precompress: false,
-			// strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		// }
 	},
+
 	compilerOptions: {
-		runes: true
+		runes: true // Force runes
 	},
+
 	vitePlugin: {
 		dynamicCompileOptions({ filename }) {
+			// Ignore runes for Shadcn and packages
 			if (filename.includes('node_modules') || filename.includes('components/ui')) {
 				return {
 					runes: false
